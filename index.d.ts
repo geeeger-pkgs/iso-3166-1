fc85b15fc9a1f9c45310478d8785ab248c699419
geeeger <lph1988520@sina.com>
2020-04-24 12:04:59 +0800

    publish(index.js)

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+declare type ISO31661Map = {
+    [key: string]: {
+        en: string;
+        es: string;
+        zh: string;
+        locale: string;
+        code: number;
+    };
+}
+
+export default ISO31661Map;
+export {};
